062c2922c680bf4706f898f0766d3726c85b74bd
offsuit2 <48610075+offsuit2@users.noreply.github.com>
2023-01-17 11:04:39 -0800

    built the other routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from './Pages/Home';
+import { Kimmy } from './Pages/Kimmy/Kimmy';
+import { Doug } from './Pages/Doug/Doug';
+import { Niko } from './Pages/Niko/Niko';
+import { David } from './Pages/David/David';
+import { Ghaslin } from './Pages/Ghaslin/Ghaslin';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,6 +15,11 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />}/>
+      <Route path='/Kimmy' element={<Kimmy />}/>
+      <Route path='/Doug' element={<Doug />}/>
+      <Route path='/Niko' element={<Niko />}/>
+      <Route path='/David' element={<David />}/>
+      <Route path='/Ghaslin' element={<Ghaslin />}/>
     </Routes>
   </BrowserRouter>
 );
