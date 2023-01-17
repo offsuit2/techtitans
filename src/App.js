6d03c187b84977de4d107da0a168e67e4cf12425
offsuit2 <48610075+offsuit2@users.noreply.github.com>
2023-01-17 10:26:31 -0800

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
+import { Navbar } from "./Components/Navbar";
 function App() {
   return (
     <div className="App">
-      <h1>HELLO</h1>
+      <Navbar />
     </div>
   );
 }
 
 export default App;
-
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+export const Navbar = () => {
+  return (
+    <>
+     <nav>
+       <button> Home</button>
+       <button>Niko</button>
+       <button>Doug</button>
+       <button>Kimmy</button>
+       <button>Ghaslin</button>
+       <button>David</button>
+     </nav>
+    </>
+  )
+}
